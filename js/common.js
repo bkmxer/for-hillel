5494c2b4fddccabdd686a13cefe3e0d54bea10ea
bkmxer <ilchuk_anton@mail.ru>
2016-07-14 17:43:16 +0300

    Сделал переключения вкладок, не работает бутстрап

--- js/common.js
+++ js/common.js
@@ -1,33 +1,10 @@
 $(document).ready(function() {
 
-	//Цели для Яндекс.Метрики и Google Analytics
-	$(".count_element").on("click", (function() {
-		ga("send", "event", "goal", "goal");
-		yaCounterXXXXXXXX.reachGoal("goal");
-		return true;
-	}));
 
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	};
-
-	//Аякс отправка форм
-	//Документация: http://api.jquery.com/jquery.ajax/
-	$("#form").submit(function(e) {
-		e.preventDefault;
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			alert("Спасибо за заявку!");
-			setTimeout(function() {
-				$.fancybox.close();
-			}, 1000);
-		});
-	});
-	
 });
+
+$(".tabs_header .wrapper .tab").click(function(){
+	$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
+
+}).eq(0).addClass("active");
